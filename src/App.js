f4ba6375056352a4ea4728320c35572a3435717e
dranent <kklamport@gmail.com>
2023-01-08 19:53:14 +0900

    20230108 commit

--- src/App.js
+++ src/App.js
@@ -26,6 +26,23 @@ function App() {
   const [open7, setOpen7] = useState(false);
   const [open8, setOpen8] = useState(false);
 
+  const [open9, setOpen9] = useState(false);
+  const [open10, setOpen10] = useState(false);
+  const [open11, setOpen11] = useState(false);
+  const [open12, setOpen12] = useState(false);
+  const [open13, setOpen13] = useState(false);
+  const [open14, setOpen14] = useState(false);
+  const [open15, setOpen15] = useState(false);
+  const [open16, setOpen16] = useState(false);
+  const [open17, setOpen17] = useState(false);
+  const [open18, setOpen18] = useState(false);
+  const [open19, setOpen19] = useState(false);
+  const [open20, setOpen20] = useState(false);
+  const [open21, setOpen21] = useState(false);
+  const [open22, setOpen22] = useState(false);
+  const [open23, setOpen23] = useState(false);
+
+
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -391,9 +408,376 @@ function App() {
       <Container className='qna-area padding-left padding-top padding-right' id='qna-a'>
         <Row className='margin-1em'><div>
           <h4>よくある質問</h4>
-
         </div>
         </Row>
+        <Row className='margin-1em'><div>
+        <h5 className='qna-sub-title'>施術について</h5>
+        </div>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen9(!open9)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open9}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : どのくらい持続しますか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open9 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open9} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : 個人差はありますが、大体1年から2年にかけて代謝と共に薄くなっていきます。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen10(!open10)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open10}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : メイクはいつから大丈夫ですか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open10 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open10} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : カサブタが取れたら可能です。7日から10日で取れます。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+        
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen11(!open11)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open11}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : 施術中の痛みはありますか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open11 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open11} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : 痛みは個人差があります。その日の体調が影響しますので前日にアルコールを控えて睡眠をよくとることをおすすめします。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen12(!open12)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open12}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : 施術後は腫れますか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open12 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open12} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : 眉毛の腫れはほぼありません。アイラインとリップは翌日まで腫れることがございます。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen13(!open13)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open13}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : 定着までに何回施術が必要ですか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open13 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open13} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : 施術は2回で定着になります。皮膚タイプや過去の残り具合によって3回目以降が必要な場合もございます。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen14(!open14)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open14}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : 施術によるリスクはありますか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open14 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open14} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : 色素は時間と共に70％から90％消えていきますが、100％消えるわけではありませんのでご了承ください。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen15(!open15)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open15}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : MRIは大丈夫ですか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open15 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open15} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : MRIを受けることは問題ありませんが、事前報告は必要になります。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen16(!open16)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open16}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : 肌が弱いのですが施術は可能ですか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open16 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open16} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : はい、可能です。カウンセリング時に
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen17(!open17)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open17}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : アトピーでも施術は可能ですか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open17 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open17} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : 予約前にご確認致します。ひどい方は施術を受けれない場合もございます。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen18(!open18)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open18}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : 金属アレルギーでも施術は可能ですか
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open18 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open18} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : 基本的に問題はございませんが、アレルギーをお持ちの方は申し出てください。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen19(!open19)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open19}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : 持病や飲んでいる薬によって施術が出来ない可能性はありますか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open19 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open19} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : はい、休薬が必要な成分がございますので飲んでる薬があれば予約前にご確認お願い致します。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen20(!open20)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open20}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : 妊娠中は施術を受けられますか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open20 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open20} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : 妊娠中の方は麻酔を使用しますので施術は受けれません。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen21(!open21)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open21}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : 男性も施術は可能ですか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open21 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open21} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : もちろんです。おすすめします。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen22(!open22)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open22}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : アフターケアですることはありますか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open22 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open22} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : 施術部位を触らないようご注意ください。
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'>
+          <Button
+            className='text-left'
+            onClick={() => setOpen23(!open23)}
+            aria-controls="example-collapse-text text-left"
+            aria-expanded={open23}
+            variant="light"
+          >
+            <div className='text-center'>
+              <h6 className='qna-q'>
+              Q : 洗顔・入浴はいつから大丈夫ですか？
+              </h6>
+              <AiFillCaretDown className={`qna-arrow ${open23 ? 'open' : ''}`} />
+            </div>
+          </Button>
+          <Collapse in={open23} className='margin-top-1em'>
+            <div id="example-collapse-text">
+              <h6>
+              A : 3日間施術部位に水や洗顔など直接当たらないように気を付けてください。入浴、サウナ、プール等は5日間避けてください。（シャワーは大丈夫です。）
+              </h6>
+            </div>
+          </Collapse>
+        </Row>
+
+        <Row className='margin-1em'><div>
+        <h5 className='qna-sub-title'>教育について</h5>
+        </div>
+        </Row>
+        
         <Row className='margin-1em'>
           <Button
             className='text-left'

3399d2598d81ea5be6963b921362517c36df45bc
dranent <kklamport@gmail.com>
2023-02-06 17:49:10 +0900

    20230206 commit3

--- src/App.js
+++ src/App.js
@@ -585,10 +585,10 @@ function App() {
       
       <div className='profile-img'>
         <h4 className='profile-name'>{profileName}</h4>
-          <img src='./img/sefa-profile_75.png' className='profile-img' alt='asami-sefa' />
-        </div> 
-        <div className='profile'>
-          
+        <img src='./img/sefa-profile_75.png' className='profile-img' alt='asami-sefa' />
+      </div> 
+
+        <div className='profile'>  
           <h6 className='display-enter'>{profileContent}</h6>
         </div>
 

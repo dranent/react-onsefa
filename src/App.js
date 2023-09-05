634a0d3c4480e29cd0b05e37896bb64ebf1d529b
KevinKlamport <kklamport@gmail.com>
2023-09-05 18:23:41 +0900

    20230905 commit4

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ import Menu from './components/menu';
 import { ReserveAcademy } from './components/ReserveAcademy';
 import { Influencer } from './components/influencer';
 import ReservationRenewal from './components/ReservationRenewal';
+import Privacy from './components/privacy';
 // import VIPPrice from './components/vip-price';
 // import Directors from './components/Directors.js';
 
@@ -73,6 +74,7 @@ function App() {
             <PriceAcademy />
             <QnAAcademy />
             <ReserveAcademy/>
+            <Privacy/>
           <Access />
           <Footer />
           <div className='bottom-margin'>

8e86689c2cbf59fb6db70da8562cbd72020f1fb1
KevinKlamport <kklamport@gmail.com>
2023-09-05 18:18:28 +0900

    20230905 commit3

--- src/components/menu.js
+++ src/components/menu.js
@@ -9,44 +9,52 @@ function Menu() {
             </div>
             <div className="AboutMenuPhotosContainer">
                 <div className="AboutMenuPhotoContainer">
-                    <img
-                        src="/img/menu/1.webp"
-                        alt="About Menu 1"
-                        className="AboutMenuPhoto"
-                    />
+                    <a href="/res/">
+                        <img
+                            src="/img/menu/1.webp"
+                            alt="About Menu 1"
+                            className="AboutMenuPhoto"
+                        />
+                    </a>
                     <div className="AboutMenuTextContainer">
                         <h4>Natural</h4>
                         <h6>お客様の毛並みに合わせて一本一本毛を表現する技法です。</h6>
                     </div>
                 </div>
                 <div className="AboutMenuPhotoContainer">
-                    <img
-                        src="/img/menu/2.webp"
-                        alt="About Menu 2"
-                        className="AboutMenuPhoto"
-                    />
+                    <a href="/res/">
+                        <img
+                            src="/img/menu/2.webp"
+                            alt="About Menu 2"
+                            className="AboutMenuPhoto"
+                        />
+                    </a>
                     <div className="AboutMenuTextContainer">
                         <h4>Powder</h4>
                         <h6>ふんわりお化粧をしたようにグラデーションをかける技法です。</h6>
                     </div>
                 </div>
                 <div className="AboutMenuPhotoContainer">
-                    <img
-                        src="/img/menu/3.webp"
-                        alt="About Menu 3"
-                        className="AboutMenuPhoto"
-                    />
+                    <a href="/res/">
+                        <img
+                            src="/img/menu/3.webp"
+                            alt="About Menu 3"
+                            className="AboutMenuPhoto"
+                        />
+                    </a>
                     <div className="AboutMenuTextContainer">
                         <h4>Combo</h4>
                         <h6>毛並みとパウダーのコンビネーション技法です。</h6>
                     </div>
                 </div>
                 <div className="AboutMenuPhotoContainer">
-                    <img
-                        src="/img/menu/4.webp"
-                        alt="About Menu 4"
-                        className="AboutMenuPhoto"
-                    />
+                    <a href="/res/">
+                        <img
+                            src="/img/menu/4.webp"
+                            alt="About Menu 4"
+                            className="AboutMenuPhoto"
+                        />
+                    </a>
                     <h4>Mix</h4>
                     <h6>お好みの技法を眉の状態に合わせてミックスできる技法です。</h6>
                 </div>

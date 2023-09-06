af40f9dac03254b7b7c4ca48f2c0faaabdba70f0
KevinKlamport <kklamport@gmail.com>
2023-09-06 18:42:00 +0900

    20230906 commit3

--- src/components/AboutArtist.js
+++ src/components/AboutArtist.js
@@ -9,36 +9,42 @@ function AboutArtist() {
       </div>
       <div className="AboutArtistPhotosContainer">
         <div className="AboutArtistPhotoContainer">
-          <img
-            src="/img/artist/asami.webp"
-            alt="About Artist 1"
-            className="AboutArtistPhoto AboutArtistSpecial"
-          />
+          <a href="https://www.instagram.com/sefa_artmake_on/">
+            <img
+              src="/img/artist/asami.webp"
+              alt="About Artist 1"
+              className="AboutArtistPhoto AboutArtistSpecial"
+            />
+          </a>
           <div className="AboutArtistTextContainer">
             <span>Asami (Academy)</span>
           </div>
         </div>
         <div className="AboutArtistPhotoContainer">
-          <img
-            src="/img/artist/okubo.webp"
-            alt="About Artist 4"
-            className="AboutArtistPhoto"
-          />
+          <a href="https://www.instagram.com/on_miyu_artmake/">
+            <img
+              src="/img/artist/okubo.webp"
+              alt="About Artist 4"
+              className="AboutArtistPhoto"
+            />
+          </a>
           <div className="AboutArtistTextContainer">
             <span>Miyu</span>
           </div>
         </div>
         <div className="AboutArtistPhotoContainer">
-          <img
-            src="/img/artist/natsumi.webp"
-            alt="About Artist 2"
-            className="AboutArtistPhoto"
-          />
+          <a href="https://www.instagram.com/on_natsumi_artmake/">
+            <img
+              src="/img/artist/natsumi.webp"
+              alt="About Artist 2"
+              className="AboutArtistPhoto"
+            />
+          </a>
           <div className="AboutArtistTextContainer">
             <span>Natsumi</span>
           </div>
         </div>
- 
+
 
 
       </div>
